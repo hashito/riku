8e35a4115e46f9cf28fc2cdbc6b2df154ec1b72f
hashito <hasitozzz@gmail.com>
2021-10-24 21:09:48 +0900

    2021-10-24

--- date/2021-10-24/main.py
+++ date/2021-10-24/main.py
@@ -1,6 +1,9 @@
 from tkinter import *
 
 win = Tk()
-cv = Canvas(win, width=400, height=300)
+cv = Canvas(win, width=380, height=380)
 cv.pack()
+for i in range(19):
+    y = i * 20
+    cv.create_line(0, y, 380, y, fill="black", width = 2)
 win.mainloop()

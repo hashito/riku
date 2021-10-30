d5002f1873a0f9aff1795375fa4a1d29d9a9117b
hashito <hasitozzz@gmail.com>
2021-10-30 10:01:52 +0900

    a

--- date/2021-10-24/test.py
+++ date/2021-10-24/test.py
@@ -1,2 +1,8 @@
-for i in range(0):
-    print(i)
+for i in range(10):
+    for j in range(10):
+        r = str(i) + "*" + str(j)
+        if not i*j == 0: 
+            print(r + "=" + str(i*j))
+        else: 
+            print(str(i) + 'の段')  
+
